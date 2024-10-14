23375f7ec9391c40bde7e47bdb4886d34adb96fd
Jhony <jonatas11oliveira@live.com>
2024-10-14 15:05:47 -0300

    Inicializando projeto

---
+++ projeto-dio-2024/projeto-dio-2024/src/main/java/com/dio/ProjetoDio2024Application.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDio2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDio2024Application.class, args);
+	}
+
+}
---
+++ projeto-dio-2024/projeto-dio-2024/src/test/java/com/dio/ProjetoDio2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDio2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
